65980d65cb088ad4d3de22ef52e0e2f30e0e49a7
Ruud <r.vanbuul@bedandbreakfast.eu>
2023-06-05 17:32:01 -0600

    ci: test ci

--- src/example.spec.ts
+++ src/example.spec.ts
@@ -1,6 +1,6 @@
 import { add } from './example';
 
-describe(add.name, function() {
+describe(add.name, () => {
     it('Should add up first and second argument', () => {
         expect(add(1, 2)).toEqual(3);
         expect(add(2, 3)).toEqual(5);
