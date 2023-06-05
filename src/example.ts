8d1f2577ce0edda7d3d764a6cfe3093518cbbdf6
Ruud <r.vanbuul@bedandbreakfast.eu>
2023-06-05 17:54:44 -0600

    ci: test ci

--- src/example.ts
+++ src/example.ts
@@ -3,3 +3,9 @@ export function add(first: number, second: number) {
 
     return first + second;
 }
+
+export function multiply(first: number, second: number) {
+    const a = 'a';
+
+    return first * second;
+}
