d5331dffcf6e297796c02a00f75c38d3ab84134b
Ruud <r.vanbuul@bedandbreakfast.eu>
2023-06-05 14:01:26 -0600

    chore: check if pre-commit gets triggered

--- src/example.ts
+++ src/example.ts
@@ -1,14 +1,3 @@
 export function add(first: number, second: number) {
-    const a = 'a';
-    const b = 'b';
-    const c = 'c';
-    const d = 'd';
-    const e = 'e';
-    const f = 'f';
-    const g = 'g';
-    const h = 'h';
-
-    console.log('HEHO');
-
     return first + second;
 }
